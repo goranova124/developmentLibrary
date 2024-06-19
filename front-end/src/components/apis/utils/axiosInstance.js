65d706c0dee6fb225357ff03600286730335294b
Denitsa Goranova <Denitsa.Goranova@DAFTRUCKS.com>
2024-06-19 13:45:49 +0200

    Updates

--- front-end/src/components/apis/utils/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseURL = 'https://primary:XXDWQ7wEBHKIfOCQLRPiCwLTiva4JIvYYX83wgAHb3oSy21e1mlAjeteFqhN593H@dpli.test.azuremicroservices.io/test/default/';
-const axiosInstance = axios.create({
-  baseURL
-});
-
-export default axiosInstance;
