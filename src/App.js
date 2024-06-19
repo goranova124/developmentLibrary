fc37c65c2dda1caf6469c0a513bca50fa82c2581
Denitsa Goranova <Denitsa.Goranova@DAFTRUCKS.com>
2024-06-19 15:45:47 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
       <Router>
         <Routes>
-          <Route path="/developmentLibrary"  element={<Home />} />
+          <Route path="/"  element={<Home />} />
           <Route path="apis/:api" element={<Vehicle />} />
 
           <Route path="documentation" element={<Documentation />} />
