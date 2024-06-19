5b314b53923e87350c1df27b6b6c81ced58e163c
Denitsa Goranova <Denitsa.Goranova@DAFTRUCKS.com>
2024-06-19 16:12:35 +0200

    Updates

--- src/components/CodeTable.js
+++ src/components/CodeTable.js
@@ -18,13 +18,10 @@ import { VehicleAPI } from "./apis/vehiclesApi";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import Snackbar from '@mui/material/Snackbar';
-import beautify, { js } from 'js-beautify';
+import beautify from 'js-beautify';
 import { ayuLight } from 'thememirror';
 import { jwtDecode } from 'jwt-decode'
 import { useCookies } from 'react-cookie';
-import { CookieProvider, CookiesProvider } from "../components/CookieProvider";
-import SyncLoader from "react-spinners/ClipLoader";
-
 
 
 function CodeTable(api) {
@@ -33,21 +30,10 @@ function CodeTable(api) {
     const [errorCode, setErrorCode] = React.useState([]);
     const [openDrawer, setOpenDrawer] = React.useState([]);
     const [authorization, setAuthorization] = React.useState('');
-    const [moreDataAvailableLink, setOpenMoreDataAvailableLink] = React.useState('');
     const [open, setOpen] = React.useState(false);
-    const [openMoreDataAvailable, setOpenMoreDataAvailable] = React.useState(false);
     const [nameError, setNameError] = useState(false);
     const [parametersList, setParametersList] = React.useState([]);
-    const [cookies, setCookie] = useCookies(['apis', 'email']);
-    const [loading, setLoading] = useState(true)
-    // const [cookies] = useCookies(['email']);
-
-    const cookieProvide = CookieProvider();
-    useEffect(() => {
-        setVehicles()
-        setValueAPI("4");
-        setErrorCode()
-    }, [api]);
+    const [cookies, setCookie] = useCookies('apis', 'email');
 
     useEffect(() => {
         setParametersList(api.api.requestParametersList)
@@ -55,8 +41,7 @@ function CodeTable(api) {
         if (token) {
             setAuthorization(token)
         }
-        console.log(cookies.token);
-    }, [cookies, authorization]);
+    }, []);
     const apiChange = (event, newValue) => {
         setValueAPI(newValue);
     };
@@ -77,110 +62,83 @@ function CodeTable(api) {
     const onCopyText = () => {
         setOpen(true);
     };
-    const getMoreDataAvailable = async (event) => {
-        setLoading(true)
-        try {
 
-            const accept = `${originalList.find(param => param.parameterName === 'Accept')?.parameterExample || ''}`;
-            let data = await VehicleAPI.getMoreData(moreDataAvailableLink, authorization, accept);
-            // const json = await data.json();
-            setVehicles(data);
-            if (data.moreDataAvailable) {
-                setOpenMoreDataAvailableLink(data.moreDataAvailableLink)
-                setOpenMoreDataAvailable(true)
-            }
-            else {
-                setOpenMoreDataAvailable(false)
-
-            }
-        }
-        catch (error) {
-            console.error('Error fetching apis list:', error);
-        } finally {
-            setLoading(false);
-        }
-
-
-    };
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
         }
         setOpen(false);
     };
-    const handleCloseMoredataAvailable = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setOpenMoreDataAvailable(false);
-    };
 
     const callApi = async (event) => {
-        event.preventDefault();
         let data;
-        
+        event.preventDefault();
+        console.log(api.api.apiFunction);
         const email1 = `${originalList.find(param => param.parameterName === 'Email')?.parameterExample || ''}`;
         const password1 = `${originalList.find(param => param.parameterName === 'Password')?.parameterExample || ''}`;
-        const accept = `${originalList.find(param => param.parameterName === 'Accept')?.parameterExample || ''}`;
-        const contentType = `${originalList.find(param => param.parameterName === 'Content-Type')?.parameterExample || ''}`;
-        const triggerFilter = `${originalList.find(param => param.parameterName === 'triggerFilter')?.parameterExample || ''}`;
-        const contentFilter = `${originalList.find(param => param.parameterName === 'contentFilter')?.parameterExample || ''}`;
-        const dateType = `${originalList.find(param => param.parameterName === 'dateType')?.parameterExample || ''}`;
-        const vin = `${originalList.find(param => param.parameterName === 'vin')?.parameterExample || ''}`;
-        const starttime = `${originalList.find(param => param.parameterName === 'starttime')?.parameterExample || ''}`;
-        const stoptime = `${originalList.find(param => param.parameterName === 'stoptime')?.parameterExample || ''}`;
-      
         if (api.api.apiFunction === "AUTHORIZATION") {
-          try {
-            data = await VehicleAPI.login(email1, password1);
-            // const json = await data.json();
-            setCookie("token", data.access_token, { expires: new Date(Date.now() + 30 * 60 * 1000) });
-            setCookie("email", (jwtDecode(data.access_token)).email, { expires: new Date(Date.now() + 30 * 60 * 1000) });
-            setVehicles(data);
-            setValueAPI("5");
-            setErrorCode(data.status);
-          } catch (error) {
-            setVehicles(null);
-            setValueAPI("5");
-            setErrorCode(error.message);
-            console.log(error);
-          }
-        } else {
-          try {
-            if (api.api.apiFunction === "GET_VEHICLES") {
-              data = await VehicleAPI.getAll(authorization, accept, contentType);
-            } else if (api.api.apiFunction === "GET_VEHICLES_POSITIONS with latestOnly") {
-              data = await VehicleAPI.getVehiclePositionByLatestOnly(authorization, accept, vin, triggerFilter, dateType);
-            } else if (api.api.apiFunction === "GET_VEHICLES_POSITIONS with starttime") {
-              data = await VehicleAPI.getVehiclePositionByStartTime(authorization, accept, vin, triggerFilter, dateType, starttime, stoptime);
-            } else if (api.api.apiFunction === "GET_VEHICLES_STATUSES with latestOnly") {
-              data = await VehicleAPI.getVehiclesStatusesByLatestOnly(authorization, accept, vin, triggerFilter, dateType, contentFilter);
-            } else if (api.api.apiFunction === "GET_VEHICLES_STATUSES with starttime") {
-              data = await VehicleAPI.getVehiclesStatusesByStartime(authorization, accept, vin, triggerFilter, dateType, contentFilter, starttime, stoptime);
-            } else {
-              throw new Error("Invalid unique function value");
+
+            try {
+
+                data = await VehicleAPI.login(email1, password1);
+                const json = await data.json();
+                setCookie("token", json.access_token, { expires: new Date(Date.now() + 30 * 60 * 1000) });
+                setCookie("email", (jwtDecode(json.access_token)).email, { expires: new Date(Date.now() + 30 * 60 * 1000) });
+                setVehicles(json);
+                setValueAPI("5");
+                setErrorCode(data.status);
             }
-            
-            // const json = await data.json();
-            if (data.moreDataAvailable) {
-              setOpenMoreDataAvailableLink(data.moreDataAvailableLink)
-              setOpenMoreDataAvailable(true)
+            catch (error) {
+                setVehicles(null);
+                setValueAPI("5");
+                setErrorCode(error.message);
+                console.log(error);
             }
-            setVehicles(data);
-            setValueAPI("5");
-            setErrorCode(data.status);
-            setNameError(false);
-          } catch (error) {
-            setVehicles(null);
-            setValueAPI("5");
-            setErrorCode(error.message);
-            console.log(error);
-          }
         }
-        
-        setLoading(false);
-      };
-      
+        else if (api.api.apiFunction !== "AUTHORIZATION") {
+            console.log(originalList);
+            const accept = `${originalList.find(param => param.parameterName === 'Accept')?.parameterExample || ''}`;
+            const contentType = `${originalList.find(param => param.parameterName === 'Content-Type')?.parameterExample || ''}`;
+            const triggerFilter = `${originalList.find(param => param.parameterName === 'triggerFilter')?.parameterExample || ''}`;
+            const contentFilter = `${originalList.find(param => param.parameterName === 'contentFilter')?.parameterExample || ''}`;
+            const datType = `${originalList.find(param => param.parameterName === 'dateType')?.parameterExample || ''}`;
+            const vin = `${originalList.find(param => param.parameterName === 'vin')?.parameterExample || ''}`;
+            const starttime = `${originalList.find(param => param.parameterName === 'starttime')?.parameterExample || ''}`;
+            const stoptime = `${originalList.find(param => param.parameterName === 'stoptime')?.parameterExample || ''}`;
+            console.log(contentFilter);
+
+            try {
+                if (api.api.apiFunction === "GET_VEHICLES") {
+                    data = await VehicleAPI.getAll(authorization, accept, contentType);
+                }
+                else if (api.api.apiFunction === "GET_VEHICLES_POSITIONS with latestOnly") {
+                    console.log(accept);
+                    data = await VehicleAPI.getVehiclePositionBylatestOnly(authorization, accept, vin, triggerFilter, datType);
+                }
+                else if (api.api.apiFunction === "GET_VEHICLES_POSITIONS with starttime") {
+                    data = await VehicleAPI.getVehiclePositionByStartTime(authorization, accept, vin, triggerFilter, datType, starttime, stoptime);
+                } else if (api.api.apiFunction === "GET_VEHICLES_STATUSES with latestOnly") {
+                    data = await VehicleAPI.getVehiclesStatusesBylatestOnly(authorization, accept, vin, triggerFilter, datType, contentFilter);
+                } else if (api.api.apiFunction === "GET_VEHICLES_STATUSES with starttime") {
+                    console.log(triggerFilter);
+                    data = await VehicleAPI.getVehiclesStatusesByStartime(authorization, accept, vin, triggerFilter, datType, contentFilter, starttime,stoptime);
+                }
+                else {
+                    throw new Error("Invalid unique function value");
+                }
+                const json = await data.json();
+                setVehicles(json);
+                setValueAPI("5");
+                setErrorCode(data.status);
+                setNameError(false);
+            } catch (error) {
+                setVehicles(null);
+                setValueAPI("5");
+                setErrorCode(error.message);
+                console.log(error);
+            }
+        }
+    };
 
     const accordionClicked = (index) => {
         if (openDrawer.includes(index))
@@ -240,52 +198,30 @@ function CodeTable(api) {
                     </Box>
                 </TabPanel>
                 <TabPanel value="5">
-                    {loading ? (
-                        <div>
-                            <p>Loading..</p>
-                            <SyncLoader
-                                color="hsla(227, 70%, 15%, 0.86)"
-                                size={25}
-                            />                                    </div>
-                    ) : (<>
-                        Response Code:
-                        <Chip label={errorCode} />
-                        <CopyToClipboard
-                            text={JSON.stringify({ vehicles })}
-                            onCopy={onCopyText}
-                        >
-                            <ContentCopyIcon />
-                        </CopyToClipboard>
-                        <Snackbar
-                            open={open}
-                            autoHideDuration={5000}
-                            onClose={handleClose}
-                            message="Code has been copied to your clipboard."
-                        />
-                        <Snackbar
-                            open={openMoreDataAvailable}
-                            autoHideDuration={5000}
-                            onClose={handleCloseMoredataAvailable}
-                            message="More data is available"
-                            action={
-                                <React.Fragment>
-                                    <Button color="secondary" size="small" onClick={getMoreDataAvailable}>
-                                        Get more data
-                                    </Button>
+                    Response Code:
+                    <Chip label={errorCode} />
+                    <CopyToClipboard
+                        text={JSON.stringify({ vehicles })}
+                        onCopy={onCopyText}
+                    >
+                        <ContentCopyIcon />
+                    </CopyToClipboard>
+                    <Snackbar
+                        open={open}
+                        autoHideDuration={5000}
+                        onClose={handleClose}
+                        message="Code has been copied to your clipboard."
+                    />
+                    <CodeMirror
+                        value={beautify(JSON.stringify(vehicles), {
+                            indent_size: 2,
+                        })}
+                        theme={ayuLight}
+                        options={{
+                            autoFormat: true
+                        }}
+                    />
 
-                                </React.Fragment>
-                            }
-                        />
-                        <CodeMirror
-                            value={beautify(JSON.stringify(vehicles), {
-                                indent_size: 2,
-                            })}
-                            theme={ayuLight}
-                            options={{
-                                autoFormat: true
-                            }}
-                        />
-                    </>)}
                 </TabPanel>
             </TabContext>
         </div>
